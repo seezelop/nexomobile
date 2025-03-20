d2cbb576d94fe5f5b2979b164fdde249b793373c
seezelop <sebastianlopez1984@gmail.com>
2025-03-20 19:12:37 -0300

    agrego estilos a pagina inicial

--- styles.js
+++ styles.js
@@ -3,13 +3,23 @@ import { StyleSheet } from 'react-native';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f1c858', // Fondo claro y moderno
     alignItems: 'center',
     justifyContent: 'center',
   },
   text: {
-    fontSize: 20,
+    fontSize: 30, // Tamaño de fuente más grande
     fontWeight: 'bold',
+    color: '#000000', // Color de texto oscuro y elegante
+    textAlign: 'center',
+    padding: 20,
+    backgroundColor: '#f6de91', // color fondo 
+    borderRadius: 10, // Bordes redondeados
+    shadowColor: '#000', // Sombra para dar profundidad
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 6,
+    elevation: 5, // Sombra en Android
   },
 });
 
