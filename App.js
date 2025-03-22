0ce6c736c9b3c7f6df038439e715cb7c86240db2
seezelop <sebastianlopez1984@gmail.com>
2025-03-22 15:23:17 -0300

    Actualizo dependencias

--- App.js
+++ App.js
@@ -1,17 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { Text, View, TouchableOpacity } from 'react-native';
-import styles from './styles';  
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import styles from './styles';
+import Login from './Login'; 
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
   return (
     <View style={styles.container}>
       <Text style={styles.text}>Bienvenido a Nexo Educativo</Text>
       
-      <TouchableOpacity style={styles.button} onPress={() => alert('¡Bienvenido!')}>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Login')}>
         <Text style={styles.buttonText}>Comenzar</Text>
       </TouchableOpacity>
 
       <StatusBar style="auto" />
     </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Login" component={Login} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
