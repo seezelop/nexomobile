e197840dcdf57d95f5e1e0acf6337f01b3f48100
seezelop <sebastianlopez1984@gmail.com>
2025-03-22 15:32:09 -0300

    Funciona navegacion inicio y login

--- Login.js
+++ Login.js
@@ -2,13 +2,13 @@ import { View, Text, TouchableOpacity } from 'react-native';
 import styles from './styles';
 
 export default function Login({ navigation }) {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Pantalla de Login</Text>
-
-      <TouchableOpacity style={styles.button} onPress={() => navigation.goBack()}>
-        <Text style={styles.buttonText}>Volver</Text>
-      </TouchableOpacity>
-    </View>
-  );
-}
+    return (
+      <View style={styles.container}>
+        <Text style={styles.text}>Pantalla de Login</Text>
+  
+        <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Home')}>
+          <Text style={styles.buttonText}>Volver</Text>
+        </TouchableOpacity>
+      </View>
+    );
+  }
