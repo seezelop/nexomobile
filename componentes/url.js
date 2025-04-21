843c795d5647b3db0c238750fe5a473f82ea5cf0
MartinaAsad <lasupermartu15@gmail.com>
2025-04-20 21:21:01 -0300

    cambue las url

--- componentes/url.js
+++ componentes/url.js
@@ -1,2 +1,2 @@
-export const API_BASE_URL = 'http://192.168.0.160:8080';
-export const BACKEND_MP =  `http://192.168.0.160:5000`
+export const API_BASE_URL = 'https://nexooeducativo-back.up.railway.app';
+export const BACKEND_MP =  `https://backend-mercado-pago-production.up.railway.app`
